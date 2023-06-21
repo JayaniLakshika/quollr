f0d848e284783f4eca3facbdf1ecbe403163c004
JayaniLakshika <jayanilakshika76@gmail.com>
2023-06-21 17:36:04 +1000

    Update hexscticker

--- hexsticker/hexsticker.R
+++ hexsticker/hexsticker.R
@@ -9,7 +9,7 @@ font_add_google("Rubik Maze", "rubik", db_cache = FALSE)
 showtext_auto()
 #img <- system.file("leaf.png", package  = "MedLEA")
 sticker(here("hexsticker/quoll_bkg.png"), package = "quollr",
-        p_size = 20, p_y = 1.65, s_x = .95, s_y =.95, s_width = .6,
+        p_size = 20, p_y = 1.5, s_x = 1, s_y =.88, s_width = .6,
         s_height = 0.3, h_fill = "#8c510a", p_color = "#f7f7f7", h_color = "#f0f0f0",
         p_family = "rubik", filename = here("hexsticker/sticker.png"))
 
