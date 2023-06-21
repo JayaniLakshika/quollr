417024b14cf6d85c06af5fd25909dde108b7a8f9
JayaniLakshika <jayanilakshika76@gmail.com>
2023-06-21 17:43:49 +1000

    Add README.qmd

--- hexsticker/hexsticker.R
+++ hexsticker/hexsticker.R
@@ -10,6 +10,6 @@ showtext_auto()
 #img <- system.file("leaf.png", package  = "MedLEA")
 sticker(here("hexsticker/quoll_bkg.png"), package = "quollr",
         p_size = 20, p_y = 1.5, s_x = 1, s_y =.88, s_width = .6,
-        s_height = 0.3, h_fill = "#8c510a", p_color = "#f7f7f7", h_color = "#f0f0f0",
+        s_height = 0.3, h_fill = "#8c510a", p_color = "#f7f7f7", h_color = "#dfc27d",
         p_family = "rubik", filename = here("hexsticker/sticker.png"))
 
