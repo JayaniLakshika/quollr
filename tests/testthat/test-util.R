69b903b7a434a6640aeb9884cd00034bbd059d5e
JayaniLakshika <jayanilakshika76@gmail.com>
2023-10-05 17:36:33 +1100

    minor fix

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -12,10 +12,10 @@ test_that("calculate_effective_shape_value() works", {
   data <- tibble::tibble(x = rnorm(300), y = rnorm(300))
   expect_equal(calculate_effective_shape_value(data, x, y), 0.96955502)
 
-  data <- data |> bind_rows(tibble(x = NA, y = 2.5))
+  data <- data |> rbind(data.frame(x = NA, y = 2.5))
   expect_error(calculate_effective_shape_value(data, x, y))
 
-  data <- data |> bind_rows(tibble(x = 1.4, y = NA))
+  data <- data |> rbind(data.frame(x = 1.4, y = NA))
   expect_error(calculate_effective_shape_value(data, x, y))
 
 })
