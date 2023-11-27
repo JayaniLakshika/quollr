13b2ae9b54e2d444236c9c413a49b62eb742be0d
JayaniLakshika <jayanilakshika76@gmail.com>
2023-11-27 13:53:42 +1100

    Add the example of the workflow

--- inst/examples/workflow.R
+++ inst/examples/workflow.R
@@ -30,26 +30,19 @@ data$x7 <- runif(sample_size, -0.01, 0.01)
 
 langevitour(data)
 
-fit_umap <- function(data, n_neighbors = NA, num_dim = 2, with_seed = NULL, ...){
-  # If a seed is specified, then use it, otherwise ignore
-  if(!is.null(with_seed)){set.seed(with_seed)}
+## Add the ID to the data
 
-  if(is.na(n_neighbors)){
-    n_neighbors <- 15
-  }
+data <- data |>
+  mutate(ID = row_number())
 
-  UMAP_fit <- data %>%
-    select(where(is.numeric)) %>%
-    umap(n_neighbors = n_neighbors, n_components =  num_dim)
+## Split the data as training and test
+data_split <- initial_split(data)
+training_data <- training(data_split) |>
+  arrange(ID)
+test_data <- testing(data_split) |>
+  arrange(ID)
 
-  UMAP_df <- UMAP_fit$layout %>%
-    as.data.frame()  %>%
-    mutate(ID=row_number())
-
-  names(UMAP_df)[1:(ncol(UMAP_df)-1)] <- paste0(rep("UMAP",(ncol(UMAP_df)-1)), 1:(ncol(UMAP_df)-1))
-  return(list(UMAP_fit = UMAP_fit, UMAP_df = UMAP_df))
-}
-
+## Fit umap
 plot_umap_2d <- function(UMAP_df){
   UMAP_df_plot <- UMAP_df %>%
     ggplot(aes(x = UMAP1,
@@ -62,214 +55,282 @@ plot_umap_2d <- function(UMAP_df){
   return(UMAP_df_plot)
 }
 
-UMAP_data <- fit_umap(data, n_neighbors = 50, with_seed = 20230531)$UMAP_df
+UMAP_fit <- umap(training_data |> dplyr::select(-ID), n_neighbors = 15, n_components =  2)
 
-data_split <- initial_split(UMAP_data)
-training_data <- training(data_split) |>
-  dplyr::arrange(ID)
-test_data <- testing(data_split) |>
-  dplyr::arrange(ID)
+UMAP_data <- UMAP_fit$layout |>
+  as.data.frame()
+names(UMAP_data)[1:(ncol(UMAP_data))] <- paste0(rep("UMAP",(ncol(UMAP_data))), 1:(ncol(UMAP_data)))
 
-plot_list2 <- plot_umap_2d(training_data) +
-  theme_linedraw() +
-  theme(plot.title = element_text(size = 7, hjust = 0.5, vjust = -0.5),
-        axis.title.x = element_blank(), axis.title.y = element_blank(),
-        axis.text.x = element_blank(), axis.ticks.x = element_blank(),
-        axis.text.y = element_blank(), axis.ticks.y = element_blank(),
-        panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+UMAP_data <- UMAP_data |>
+  mutate(ID = training_data$ID)
 
-plot_list2
+plot_umap_2d( UMAP_data)
 
+## predict umap embeddings
 
-num_bins_x <- calculate_effective_x_bins(.data = training_data, x = UMAP1,
+predict_UMAP_df <- predict(UMAP_fit, test_data |> dplyr::select(-ID)) |>
+  as.data.frame()
+
+names(predict_UMAP_df)[1:(ncol(predict_UMAP_df))] <- paste0(rep("UMAP",(ncol(predict_UMAP_df))), 1:(ncol(predict_UMAP_df)))
+
+predict_UMAP_df <- predict_UMAP_df |>
+  mutate(ID = test_data$ID)
+
+plot_umap_2d(UMAP_data) +
+  geom_point(data = predict_UMAP_df, aes(x = UMAP1, y = UMAP2), color = "red")
+
+## Calculate number of bins along x-axis
+num_bins_x <- calculate_effective_x_bins(.data = UMAP_data, x = UMAP1,
                                          cell_area = 1)
 num_bins_x
 
-shape_val <- calculate_effective_shape_value(.data = training_data,
+## Calculate shape parameter
+
+shape_val <- calculate_effective_shape_value(.data = UMAP_data,
                                              x = UMAP1, y = UMAP2)
 shape_val
 
-hexgrid_with_points <- draw_full_hexgrid(.data = data, nldr_df = training_data,
-                                         embedding_1 = UMAP1, embedding_2 = UMAP2,
-                                         num_bins = num_bins_x, shape_val = shape_val)
-hexgrid_with_points
+## To extract bin centroids
 
-df_with_hexID <- create_hexbin_df(.data = data, nldr_df = training_data,
-                               embedding_1 = UMAP1, embedding_2 = UMAP2,
-                               num_bins = num_bins_x, shape_val = shape_val)
+hexbin_data_object <-extract_hexbin_centroids(UMAP_data, num_bins_x, shape_val)
 
-df_all <- df_with_hexID$df_new
+df_bin_centroids <- hexbin_data_object$hexdf_data
 
-hb_object <- df_with_hexID$hb
+df_bin_centroids |>
+  head() |>
+  DT::datatable()
+
+## Data set with all possible centroids in the hexagonal grid
+
+full_centroid_df <- generate_full_grid_centroids(df_bin_centroids)
+
+full_centroid_df |>
+  head(5) |>
+  DT::datatable()
+
+## Generate all coordinates of hexagons
+hex_grid <- full_hex_grid(full_centroid_df)
+
+ggplot(data = hex_grid, aes(x = x, y = y)) + geom_polygon(fill = "white", color = "black", aes(group = id)) +
+  geom_point(data = hex_grid, aes(x = x, y = y), color = "black") +
+  geom_point(data = df_bin_centroids, aes(x = x, y = y), color = "red")
+
+ggplot(data = hex_grid, aes(x = x, y = y)) + geom_polygon(fill = "white", color = "black", aes(group = id)) +
+  geom_point(data = full_centroid_df, aes(x = x, y = y), color = "black") +
+  geom_point(data = df_bin_centroids, aes(x = x, y = y), color = "red")
+
+ggplot(data = hex_grid, aes(x = x, y = y)) + geom_polygon(fill = "white", color = "black", aes(group = id))
+
+
+hexdf_data_c <- df_bin_centroids |>
+  rename("c_x" = "x",
+         "c_y" = "y")
+
+hex_full_count_df <- full_join(hex_grid, hexdf_data_c, by = c("id" = "hexID")) |>
+  dplyr::mutate(std_counts = tidyr::replace_na(std_counts, 0))
+
+ggplot(data = hex_full_count_df, aes(x = x, y = y)) +
+  geom_polygon(color = "black", aes(group = id, fill = std_counts)) +
+  scale_fill_viridis_c(direction = -1)
+
+## Add hexbin Id to 2D embeddings
+UMAP_data_with_hb_id <- UMAP_data |>
+  mutate(hb_id = hexbin_data_object$hb_data@cID)
+
+## To generate data set with point info
+
+pts_df <- data.frame(matrix(ncol = 0, nrow = 0))
+
+for (i in 1:length(UMAP_data_with_hb_id$hb_id)) {
+
+  pts_vec <- UMAP_data_with_hb_id |>
+    dplyr::filter(hb_id == UMAP_data_with_hb_id$hb_id[i]) |>
+    dplyr::pull(ID)
+
+  hb_pts <- tibble::tibble(hexID = UMAP_data_with_hb_id$hb_id[i], pts = list(pts_vec))
+
+  pts_df <- dplyr::bind_rows(pts_df, hb_pts)
+
+}
+
+pts_df <- dplyr:inner_join(df_bin_centroids, pts_df, by = c("hexID" = "hexID")) |>
+  dplyr::distinct()
+
+pts_df |>
+  head() |>
+  DT::datatable()
+
+## To generate a data set with high-D and 2D training data
+df_all <- dplyr::bind_cols(training_data |> dplyr::select(-ID), UMAP_data_with_hb_id)
 
 df_all |>
-  datatable()
+  head() |>
+  DT::datatable()
+
+## To generate averaged high-D data
 
 df_bin <- avg_highD_data(.data = df_all)
 
 df_bin |>
-  datatable()
+  head() |>
+  DT::datatable()
 
-df_bin_centroids <- extract_hexbin_centroids(.data = df_bin, hb = hb_object)
+## Triangulate bin centroids
 
-df_bin_centroids |>
-  datatable()
-
-tr1_object <- triangulate_bin_centroids(df_bin_centroids, x_val_center, y_val_center)
-
+tr1_object <- triangulate_bin_centroids(df_bin_centroids, x, y)
 tr_from_to_df <- generate_edge_info(triangular_object = tr1_object)
 
+tr_from_to_df |>
+  head() |>
+  DT::datatable()
+
+## Compute 2D distances
 distance <- cal_2D_dist(.data = tr_from_to_df)
 
+## To plot the distribution of distance
+plot_dist <- function(distance_df){
+  distance_df$group <- "1"
+  dist_plot <- ggplot(distance_df, aes(x = group, y = distance)) +
+    geom_quasirandom()+
+    ylim(0, max(unlist(distance_df$distance))+ 0.5) + coord_flip()
+  return(dist_plot)
+}
+
 distance_plot <- plot_dist(distance) +
+  #ggtitle("(b)" ) +
   ylab(expression(d^{(2)})) +
   theme(axis.text = element_text(size = 5),
         axis.title = element_text(size = 12))
 
 distance_plot
 
+
+## To draw the traingular mesh
+
+trimesh <- ggplot(df_bin_centroids, aes(x = x, y = y)) +
+  geom_point(size = 0.1) +
+  geom_trimesh() +
+  coord_equal()
+
+trimesh <- trimesh +
+  #ggtitle("(a)") +
+  xlab(expression(C[x]^{(2)})) + ylab(expression(C[y]^{(2)})) +
+  theme(axis.text = element_text(size = 5),
+        axis.title = element_text(size = 7))
+
+trimesh
+
+## To find the benchmark value
+
 benchmark <- find_benchmark_value(.data = distance, distance_col = distance)
 benchmark
 
+## To draw the colored long edges in 2D
+
 trimesh_gr <- colour_long_edges(.data = distance, benchmark_value = benchmark,
                                 triangular_object = tr1_object, distance_col = distance)
+
+trimesh_gr <- trimesh_gr +
+  xlab(expression(C[x]^{(2)})) + ylab(expression(C[y]^{(2)})) +
+  theme(axis.text = element_text(size = 5),
+        axis.title = element_text(size = 7))
+
 trimesh_gr
+
+## To draw after removing long edges in 2D
 
 trimesh_removed <- remove_long_edges(.data = distance, benchmark_value = benchmark,
                                      triangular_object = tr1_object, distance_col = distance)
+
+trimesh_removed <- trimesh_removed +
+  xlab(expression(C[x]^{(2)})) + ylab(expression(C[y]^{(2)})) +
+  theme(axis.text = element_text(size = 5),
+        axis.title = element_text(size = 7))
+
 trimesh_removed
 
 tour1 <- show_langevitour(df_all, df_bin, df_bin_centroids, benchmark_value = benchmark, distance = distance, distance_col = distance)
 tour1
 
+
 ## Prediction
 
-### Option 1
+shape_value <- calculate_effective_shape_value(.data = UMAP_data,
+                                               x = UMAP1, y = UMAP2)
 
-num_bins_vec <- append(sample(2:150, 40), num_bins_x)
+num_bins_vec <- append(sample(1:50, 40), num_bins_x)
 
-vec <- stats::setNames(rep("", 4), c("number_of_bins", "number_of_observations", "total_error_method_1", "total_error_method_2"))  ## Define column names
-prediction_data <- dplyr::bind_rows(vec)[0, ]
+vec <- stats::setNames(rep("", 5), c("number_of_bins", "number_of_observations", "total_error", "totol_error_method_2", "totol_error_method_3"))  ## Define column names
 
-prediction_data <- prediction_data |>
+eval_data_test <- dplyr::bind_rows(vec)[0, ]
+eval_data_test <- eval_data_test |>
   dplyr::mutate_if(is.character, as.numeric)
 
-prediction_data_training <- dplyr::bind_rows(vec)[0, ]
-
-prediction_data_training <- prediction_data_training |>
-  dplyr::mutate_if(is.character, as.numeric)
-
-for (i in 1:length(num_bins_vec)) {
-
-  predict_df <-predict_method_1(UMAP_data)
-
-  summary_df_training <- predict_df$prediction_data_training
-
-  prediction_data_training <- bind_rows(prediction_data_training, summary_df_training)
-
-  summary_df <- predict_df$prediction_data_test
-
-  prediction_data <- bind_rows(prediction_data, summary_df)
-
-
-}
-
-prediction_data |>
-  datatable()
-
-
-prediction_data <- prediction_data |>
-  mutate(data_type = "test")
-
-prediction_data_training <- prediction_data_training |>
-  mutate(data_type = "training")
-
-MSE_df <- bind_rows(prediction_data, prediction_data_training)
-
-ggplot(MSE_df, aes(x = number_of_bins,
-                   y = total_error_method_1,
-                   color = data_type
-)) +
-  #geom_line() +
-  geom_point() +
-  geom_smooth(method = "loess", se = FALSE) +
-  # scale_fill_discrete(name = "Method", labels = c("Using the number of observations in test data", "Using the number of bins")) +
-  scale_fill_manual(values = c("#1b9e77", "#d95f02")) +
-  ylab("Total MSE from method 1")
-
-
-ggplot(MSE_df, aes(x = number_of_bins,
-                   y = total_error_method_2,
-                   color = data_type
-)) +
-  #geom_line() +
-  geom_point() +
-  geom_smooth(method = "loess", se = FALSE) +
-  # scale_fill_discrete(name = "Method", labels = c("Using the number of observations in test data", "Using the number of bins")) +
-  scale_fill_manual(values = c("#1b9e77", "#d95f02")) +
-  ylab("Total MSE from method 2")
-
-
-### Option 2
-
-num_bins_vec <- append(sample(2:150, 40), num_bins_x)
-
-vec <- stats::setNames(rep("", 4), c("number_of_bins", "number_of_observations", "total_error_method_1", "total_error_method_2"))  ## Define column names
-prediction_data <- dplyr::bind_rows(vec)[0, ]
-
-prediction_data <- prediction_data |>
-  dplyr::mutate_if(is.character, as.numeric)
-
-prediction_data_training <- dplyr::bind_rows(vec)[0, ]
-
-prediction_data_training <- prediction_data_training |>
+eval_data_training <- dplyr::bind_rows(vec)[0, ]
+eval_data_training <- eval_data_training |>
   dplyr::mutate_if(is.character, as.numeric)
 
 for (i in 1:length(num_bins_vec)) {
 
-  predict_df <- predict_method_2(UMAP_data)
+  pred_df_training_object <- predict_hex_id(training_data = training_data, nldr_df = UMAP_data, nldr_df_test = UMAP_data, num_bins = num_bins_vec[i], shape_val = shape_value)
+  pred_df_training <- pred_df_training_object$pred_data
+  centroid_df_training <- pred_df_training_object$df_bin_centroids
+  avg_df_training <- pred_df_training_object$df_bin
 
-  summary_df_training <- predict_df$prediction_data_training
-  prediction_data_training <- bind_rows(prediction_data_training, summary_df_training)
+  eval_df_training <- generate_eval_df(data = data, prediction_df = pred_df_training, df_bin_centroids = centroid_df_training, df_bin = avg_df_training, num_bins = num_bins_vec[i])
 
+  pred_df_test_object <- predict_hex_id(training_data = training_data, nldr_df = UMAP_data, nldr_df_test = predict_UMAP_df, num_bins = num_bins_vec[i], shape_val = shape_value)
+  pred_df_test <- pred_df_test_object$pred_data
+  centroid_df_test <- pred_df_test_object$df_bin_centroids
+  avg_df_test <- pred_df_test_object$df_bin
 
-  ## For test data
+  eval_df_test <- generate_eval_df(data = data, prediction_df = pred_df_test, df_bin_centroids = centroid_df_test, df_bin = avg_df_test, num_bins = num_bins_vec[i])
 
-  summary_df <- predict_df$prediction_data_test
-
-  prediction_data <- bind_rows(prediction_data, summary_df)
-
+  eval_data_training <- dplyr::bind_rows(eval_data_training, eval_df_training)
+  eval_data_test <- dplyr::bind_rows(eval_data_test, eval_df_test)
 
 }
 
-prediction_data |>
-  datatable()
 
-prediction_data <- prediction_data |>
-  mutate(data_type = "test")
+## Add new column with data types
 
-prediction_data_training <- prediction_data_training |>
+eval_data_training <- eval_data_training |>
   mutate(data_type = "training")
 
-MSE_df <- bind_rows(prediction_data, prediction_data_training)
+eval_data_test <- eval_data_test |>
+  mutate(data_type = "test")
 
-ggplot(MSE_df, aes(x = number_of_bins,
-                   y = total_error_method_1,
-                   color = data_type
-)) +
-  #geom_line() +
-  geom_point() +
-  geom_smooth(method = "loess", se = FALSE) +
-  # scale_fill_discrete(name = "Method", labels = c("Using the number of observations in test data", "Using the number of bins")) +
-  scale_fill_manual(values = c("#1b9e77", "#d95f02")) +
-  ylab("Total MSE from method 1")
+MSE_df <- dplyr::bind_rows(eval_data_training, eval_data_test)
 
+## To draw with AIC
 ggplot(MSE_df, aes(x = number_of_bins,
-                   y = total_error_method_2,
+                   y = total_error,
                    color = data_type
 )) +
   geom_point() +
   geom_smooth(method = "loess", se = FALSE) +
-  # scale_fill_discrete(name = "Method", labels = c("Using the number of observations in test data", "Using the number of bins")) +
+  geom_vline(xintercept = num_bins_x) +
   scale_fill_manual(values = c("#1b9e77", "#d95f02")) +
-  ylab("Total MSE from method 2")
+  ylab("AIC")
+
+
+ggplot(MSE_df, aes(x = number_of_bins,
+                   y = totol_error_method_2,
+                   color = data_type
+)) +
+  geom_point() +
+  geom_smooth(method = "loess", se = FALSE) +
+  geom_vline(xintercept = num_bins_x) +
+  scale_fill_manual(values = c("#1b9e77", "#d95f02")) +
+  ylab("Method 2")
+
+ggplot(MSE_df, aes(x = number_of_bins,
+                   y = totol_error_method_3,
+                   color = data_type
+)) +
+  geom_point() +
+  geom_smooth(method = "loess", se = FALSE) +
+  geom_vline(xintercept = num_bins_x) +
+  scale_fill_manual(values = c("#1b9e77", "#d95f02")) +
+  ylab("Method 3")
+
