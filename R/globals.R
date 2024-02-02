4e54866f68f9f4302836c324e9a25de3f92cbe75
JayaniLakshika <jayanilakshika76@gmail.com>
2024-02-02 20:17:32 +1100

    Add function to shift the grid

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
 utils::globalVariables(c("hb_id", "UMAP1", "UMAP2", "x", "y", "x_from", "y_from",
                          "x_to", "y_to", "type", "ID", "hexID", "difference", "distance",
                          "std_counts", "from", "to", "n", "polygon_id", "counts",
-                         "df_bin_centroids", "Cell_count", "id", "distance_trans", "."))
+                         "df_bin_centroids", "Cell_count", "id", "distance_trans", ".", "c_x", "c_y"))
