826662ecfdedf404db9dcdc358f8b559db82a09f
JayaniLakshika <jayanilakshika76@gmail.com>
2024-02-27 17:01:35 +1100

    Add function to compute y_max

--- R/data_preprocessing.R
+++ R/data_preprocessing.R
@@ -1,3 +1,65 @@
+#' Compute maximum value of y for scaling
+#'
+#' This function compute the maximum y value need to use for scaling.
+#'
+#' @param aspect_ratio Numeric value representing the aspect ratio of the plot area.
+#' @param hex_ratio Numeric value representing the ratio of the hexagon size.
+#'
+#' @return A value which should be used as maximum value of y when scaling.
+#'
+#'
+#' @examples
+#' calc_y_max(aspect_ratio = 2.019414, hex_ratio = 0.2309401)
+#'
+#' @export
+calc_y_max <- function(aspect_ratio, hex_ratio) {
+
+  if (is.na(aspect_ratio) ) {
+
+    stop("Aspect ratio is missing.")
+
+  }
+
+  if (is.na(hex_ratio)) {
+
+    stop("Hex ratio is missing.")
+  }
+
+  if (is.infinite(aspect_ratio)) {
+
+    stop("Aspect ratio can't be infinite.")
+
+
+  }
+
+  if (is.infinite(hex_ratio)) {
+
+    stop("Hex ratio can't be infinite.")
+
+
+  }
+
+  if (hex_ratio <= 0) {
+
+    stop("Hex ratio can't be zero or negative.")
+
+
+  }
+
+  if (aspect_ratio <= 0) {
+
+    stop("Aspect ratio can't be zero or negative.")
+
+  }
+
+
+  ymax <- ceiling(aspect_ratio/hex_ratio) * hex_ratio
+
+  return(ymax)
+
+}
+
+
 #' Scaling the 2D embeddings
 #'
 #' This function scales the 2D embeddings.
@@ -5,28 +67,40 @@
 #' @param nldr_df A data frame containing 2D embeddings.
 #' @param x The name of the column that contains first 2D embeddings component.
 #' @param y The name of the column that contains second 2D embeddings component.
+#' @param hex_ratio Numeric value representing the ratio of the hexagon size.
 #'
 #' @return A list contains scaled 2D embeddings.
 #'
 #' @importFrom rlang as_string sym
-#' @importFrom scales rescale
 #'
 #' @examples
-#' generate_scaled_data(nldr_df = s_curve_noise_umap, x = "UMAP1", y = "UMAP2")
+#' gen_scaled_data(nldr_df = s_curve_noise_umap, x = "UMAP1", y = "UMAP2")
 #'
 #' @export
-generate_scaled_data <- function(nldr_df, x = "UMAP1", y = "UMAP2") {
+gen_scaled_data <- function(nldr_df, x, y, hex_ratio) {
 
   ## Obtain 2D embeddings
   emb1_vec <- nldr_df[[rlang::as_string(rlang::sym(x))]]
   emb2_vec <- nldr_df[[rlang::as_string(rlang::sym(y))]]
 
-  ## Scale first embedding between 0 and 1
-  scaled_emb1_vec <- scales::rescale(emb1_vec)
+  ## Compute aspect ratio
+  aspect_ratio <- abs(diff(range(emb2_vec))/diff(range(emb1_vec)))
 
-  ## Scale second embedding between -sqrt(3)/2 and sqrt(3)/2
-  y_min <- -sqrt(3)/2
-  y_max <- sqrt(3)/2
+  ## Scale first embedding between 0 and 1
+  x_min <- 0
+  x_max <- 1
+  scaled_emb1_vec <- ((emb1_vec - min(emb1_vec))/ (max(emb1_vec) - min(emb1_vec))) * (x_max - x_min)
+
+  ## Scale second embedding between 0 and ymax
+  y_min <- 0
+
+  if(is.na(ymax)) {
+    ## Default hex ratio
+    hex_ratio <- 2/sqrt(3)
+  }
+
+  y_max <- calc_y_max(aspect_ratio = aspect_ratio, hex_ratio = hex_ratio)
+
   scaled_emb2_vec <- ((emb2_vec - min(emb2_vec))/ (max(emb2_vec) - min(emb2_vec))) * (y_max - y_min)
 
   scaled_emb_list <- list(scaled_emb1_vec = scaled_emb1_vec, scaled_emb2_vec = scaled_emb2_vec)
@@ -36,3 +110,4 @@ generate_scaled_data <- function(nldr_df, x = "UMAP1", y = "UMAP2") {
 
   return(scaled_emb_list)
 }
+
