50d1abb76a4727e1d2053e08bdfb4586cb886e0a
JayaniLakshika <jayanilakshika76@gmail.com>
2025-05-12 12:30:58 +1000

    Added NLDR scaled object to the main function

--- R/model.R
+++ R/model.R
@@ -59,7 +59,7 @@ fit_highd_model <- function(highd_data, nldr_data, bin1 = 4, q = 0.1, benchmark_
 
   cli::cli_alert_success("Model generated successfully! 🎉")
 
-  return(list(hb_obj = hb_obj, model_highd = model_highd, model_2d = model_2d, trimesh_data = trimesh_data))
+  return(list(nldr_obj = nldr_obj, hb_obj = hb_obj, model_highd = model_highd, model_2d = model_2d, trimesh_data = trimesh_data))
 
 }
 
