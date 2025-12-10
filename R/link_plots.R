f1462b5ea5b71d222d0491b1399d58e3a31272ec
JayaniLakshika <jayanilakshika76@gmail.com>
2025-12-10 12:14:05 +1100

    Updated link plots

--- R/link_plots.R
+++ R/link_plots.R
@@ -368,8 +368,8 @@ show_error_link_plots <- function(point_data, edge_data,
                                                  levelColors = point_colours,
                                                  link=shared_df,
                                                  linkFilter=FALSE,
-                                                 width = "200px",
-                                                 height = "200px")
+                                                 width = "300px",
+                                                 height = "300px")
 
   linked_plt <- crosstalk::bscols(htmltools::div(style = "display: grid; grid-template-columns: 1fr 1fr 1fr;
     gap: 0px;
@@ -377,11 +377,11 @@ show_error_link_plots <- function(point_data, edge_data,
     justify-items: center;
     margin: 0;
     padding: 0;",
-    htmltools::div(style = 'margin: 0; padding: 0; height: 250px; width: 200px; text-align: center; align-items: center;',
+    htmltools::div(style = 'margin: 0; padding: 0; height: 300px; width: 200px; text-align: center; align-items: center;',
                    htmltools::h4("Distribution of residuals"), error_plt),
-    htmltools::div(style = 'margin: 0; padding: 0; height: 250px; width: 200px; text-align: center; align-items: center;',
+    htmltools::div(style = 'margin: 0; padding: 0; height: 300px; width: 200px; text-align: center; align-items: center;',
                    htmltools::h4("2-D NLDR layout"), nldr_plt),
-    htmltools::div(style = 'margin: 0; padding: 0; height: 250px; width: 200px; text-align: center; align-items: center;',
+    htmltools::div(style = 'margin: 0; padding: 0; height: 300px; width: 250px; text-align: center; align-items: center;',
                    htmltools::h4("Tour view"), htmltools::div(
                      style = "margin-top: 40px;",
                      langevitour_output
